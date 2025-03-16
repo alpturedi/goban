bdeeabc904b5c0049c80497db325b81c27f200af
alpturedi <a.k.turedi@gmail.com>
2025-03-16 20:51:34 +0200

    Board and dark mode

---
+++ components/Node.tsx
@@ -0,0 +1,12 @@
+// import { cn } from "@/lib/utils";
+
+export default function Node({}) {
+  return (
+    <div className="bg-black dark:bg-white w-20 h-20 grid grid-cols-2 grid-rows-2 gap-2">
+      <div className="bg-white dark:bg-background"></div>
+      <div className="bg-white dark:bg-background"></div>
+      <div className="bg-white dark:bg-background"></div>
+      <div className="bg-white dark:bg-background"></div>
+    </div>
+  );
+}
