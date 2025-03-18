e18370f54b2fbcd399c32ca06bb7a8b12bd45a06
Alp Turedi <alp.turedi@gmail.com>
2025-03-18 15:17:02 +0200

    Handle moves primitively

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,51 @@
-// import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 import { ModeToggle } from "@/components/ModeToggle";
 import Node from "@/components/Node";
 
-export default function Home() {
-  const board = [];
+function indexMaker(x: number, y: number) {
+  return x * 9 + y;
+}
 
+export default function Home() {
+  const [board, setBoard] = useState(Array(81).fill(null));
+  const [isBlacksTurn, setIsBlacksTurn] = useState(true);
+
+  const handleClick = (x: number, y: number) => {
+    const newBoard = board.map((node, index) => {
+      if (index === indexMaker(x, y)) {
+        return isBlacksTurn ? "black" : "white";
+      }
+      return node;
+    });
+
+    setIsBlacksTurn(!isBlacksTurn);
+    setBoard(newBoard);
+  };
+
+  const nodeArray = [];
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      board.push(<Node key={`${i}-${j}`} />);
+      nodeArray.push(
+        <Node
+          key={`${i}-${j}`}
+          onClick={() => {
+            handleClick(i, j);
+          }}
+          state={board[indexMaker(i, j)]}
+        />,
+      );
     }
   }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="grid grid-cols-9 grid-rows-9">{board}</div>
+        <div className="grid grid-cols-9 grid-rows-9">{nodeArray}</div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        {/* <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a> */}
         <ModeToggle />
       </footer>
     </div>
--- components/Node.tsx
+++ components/Node.tsx
@@ -1,8 +1,24 @@
-// import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
-export default function Node({}) {
+export default function Node({
+  onClick,
+  state,
+}: {
+  onClick: () => void;
+  state?: "white" | "black";
+}) {
   return (
-    <div className="bg-black dark:bg-white w-20 h-20 grid grid-cols-2 grid-rows-2 gap-2">
+    <div
+      onClick={onClick}
+      className="bg-black dark:bg-white w-20 h-20 grid grid-cols-2 grid-rows-2 gap-2 relative"
+    >
+      <div
+        className={cn(
+          "absolute w-16 h-16 rounded-full inset-2 z-10 border-3 border-transparent",
+          state === "black" && "bg-black border-gray-800",
+          state === "white" && "bg-white border-gray-100",
+        )}
+      />
       <div className="bg-white dark:bg-background"></div>
       <div className="bg-white dark:bg-background"></div>
       <div className="bg-white dark:bg-background"></div>
